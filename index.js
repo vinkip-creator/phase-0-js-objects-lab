e00315f3fb488ebf60e2496396e8624750c1c1cf
vinkip <vinkiplagat02@gmail.com>
2026-03-23 10:53:22 +0300

    Functions creation

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ function logAttendeeName() {
   console.log(attendeeName);
 }
 
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+} 
+
+function updateTicketType(attendee, newType) {
+  attendee.ticketType = newType;
+}
+
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 
